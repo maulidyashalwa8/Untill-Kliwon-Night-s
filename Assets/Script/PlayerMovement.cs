3b27f8bfd691ddfb75c12c903d9c99a42ac7f52f
Vanisa Rahma Yunita <vanisarahmayunita06@gmail.com>
2024-09-05 00:49:09 +0700

    rumah dan movement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+
+    private CharacterController controller;
+    private Camera playerCamera;
+    private float verticalRotation = 0;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Mengontrol pergerakan
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * moveHorizontal + transform.forward * moveVertical;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Mengontrol rotasi kamera
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
